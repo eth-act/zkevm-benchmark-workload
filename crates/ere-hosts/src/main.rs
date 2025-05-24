019407444d44d0918d035771ccac068e7645dfd0
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-24 20:30:02 +0100

    ere

--- crates/ere-hosts/src/main.rs
+++ crates/ere-hosts/src/main.rs
@@ -1,15 +1,24 @@
 //! Binary for benchmarking different Ere compatible zkVMs
 
+use std::path::PathBuf;
+
 use benchmark_runner::run_benchmark_ere;
+use ere_risczero::RV32_IM_RISCZERO_ZKVM_ELF;
+use ere_sp1::EreSP1;
+use zkvm_interface::{Compiler, ProverResourceType};
 
 /// Main entry point for the host benchmarker
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // SP1 zkVM
-    run_benchmark_ere::<ere_sp1::RV32_IM_SUCCINCT_ZKVM_ELF, ere_sp1::EreSP1>(
-        "sp1",
-        concat!(env!("CARGO_WORKSPACE_DIR"), "ere-guests/sp1"),
-    )?;
+    let sp1_zkvm = new_sp1_zkvm()?;
+    run_benchmark_ere("sp1", sp1_zkvm)?;
 
     // TODO: Add more backends
     Ok(())
+}
+
+fn new_sp1_zkvm() -> Result<EreSP1, Box<dyn std::error::Error>> {
+    let prover_resource = ProverResourceType::Cpu;
+    let guest_dir = concat!(env!("CARGO_WORKSPACE_DIR"), "ere-guests/sp1");
+    let program = ere_sp1::RV32_IM_SUCCINCT_ZKVM_ELF::compile(&PathBuf::from(guest_dir))?;
+    Ok(ere_sp1::EreSP1::new(program, prover_resource))
 }
