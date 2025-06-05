4b662fbb0e0d5d89ec09c9263369d4054c44a48e
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-06-05 15:17:15 +0200

    remove zkm

--- crates/zkevm-zkm/host/build.rs
+++
@@ -1,6 +0,0 @@
-// Remove this or modify succinct to also use this method for compiling the host
-//
-/// Build script to automatically build the guest program
-fn main() {
-    zkm_build::build_program("../program"); // Here program is the directory where the guest program is located
-}
