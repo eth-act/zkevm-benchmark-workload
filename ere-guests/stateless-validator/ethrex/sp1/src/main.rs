58af0f089bfccfafe4cdf7a4cb10046afeeb7280
Ignacio Hagopian <jsign.uy@gmail.com>
2025-09-09 18:37:04 -0300

    fix
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- ere-guests/stateless-validator/ethrex/sp1/src/main.rs
+++ ere-guests/stateless-validator/ethrex/sp1/src/main.rs
@@ -8,11 +8,14 @@ sp1_zkvm::entrypoint!(main);
 pub fn main() {
     println!("cycle-tracker-report-start: read_input");
     let input = sp1_zkvm::io::read_vec();
-    let input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
+    let mut input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
     println!("cycle-tracker-report-end: read_input");
 
     println!("cycle-tracker-report-start: public_inputs_preparation");
     let block_hash = input.blocks[0].hash();
+    // Ethrex forces the block hash of any provided block to be zero, instead of accepting pre-calculated values
+    // and just checking them again.
+    input.blocks[0].header.hash = Default::default();
     let parent_hash = input.blocks[0].header.parent_hash;
     println!("cycle-tracker-report-end: public_inputs_preparation");
 
