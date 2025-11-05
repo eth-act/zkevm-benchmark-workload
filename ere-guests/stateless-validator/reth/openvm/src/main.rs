fba722d3df8e6d7f7db8314408c8b4f565de5cc5
han0110 <tinghan0110@gmail.com>
2025-11-05 11:55:49 +0000

    feat: use `ere-platform-openvm`

--- ere-guests/stateless-validator/reth/openvm/src/main.rs
+++ ere-guests/stateless-validator/reth/openvm/src/main.rs
@@ -1,10 +1,11 @@
 //! OpenVM guest program
 
-use openvm::io::{read_vec, reveal_bytes32};
+use ere_platform_openvm::OpenVMPlatform;
 use reth_guest::{
     guest::ethereum_guest,
     sdk::{ScopeMarker, SDK},
 };
+use sha2::Sha256;
 
 // For linker declarations:
 use openvm_keccak256 as _;
@@ -14,13 +15,7 @@ openvm::init!();
 struct OpenVMSDK;
 
 impl SDK for OpenVMSDK {
-    fn read_input() -> Vec<u8> {
-        read_vec()
-    }
-
-    fn commit_output(output: [u8; 32]) {
-        reveal_bytes32(output);
-    }
+    type Platform = OpenVMPlatform<Sha256>;
 
     fn cycle_scope(_scope: ScopeMarker, _message: &str) {}
 }
