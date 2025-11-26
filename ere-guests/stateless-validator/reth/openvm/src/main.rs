738813d4d0c46202330f2063d1752a4af116fa0c
han0110 <tinghan0110@gmail.com>
2025-11-26 07:23:46 +0000

    feat: update `openvm` guests

--- ere-guests/stateless-validator/reth/openvm/src/main.rs
+++ ere-guests/stateless-validator/reth/openvm/src/main.rs
@@ -1,30 +1,14 @@
 //! OpenVM guest program
 
-use openvm::io::{read_vec, reveal_bytes32};
-use reth_guest::{
-    guest::ethereum_guest,
-    sdk::{ScopeMarker, SDK},
-};
+use ere_platform_openvm::OpenVMPlatform;
+use reth_guest::guest::ethereum_guest;
+use sha2::Sha256;
 
 openvm::init!();
-
-struct OpenVMSDK;
-
-impl SDK for OpenVMSDK {
-    fn read_input() -> Vec<u8> {
-        read_vec()
-    }
-
-    fn commit_output(output: [u8; 32]) {
-        reveal_bytes32(output);
-    }
-
-    fn cycle_scope(_scope: ScopeMarker, _message: &str) {}
-}
 
 /// Entry point.
 pub fn main() {
     openvm_revm_crypto::install_openvm_crypto()
         .expect("failed to install OpenVM revm crypto provider");
-    ethereum_guest::<OpenVMSDK>();
+    ethereum_guest::<OpenVMPlatform<Sha256>>();
 }
