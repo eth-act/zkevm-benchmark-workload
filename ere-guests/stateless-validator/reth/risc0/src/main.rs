4bbd6865fa493b9d107d4e59ff24edb110ef5c55
han0110 <tinghan0110@gmail.com>
2025-11-26 07:22:20 +0000

    feat: update `risc0` guests

--- ere-guests/stateless-validator/reth/risc0/src/main.rs
+++ ere-guests/stateless-validator/reth/risc0/src/main.rs
@@ -2,34 +2,11 @@
 
 extern crate alloc;
 
-use reth_guest::{
-    guest::ethereum_guest,
-    sdk::{ScopeMarker, SDK},
-};
-use risc0_zkvm::guest::env;
-
-pub struct Risc0SDK;
-
-impl SDK for Risc0SDK {
-    fn read_input() -> Vec<u8> {
-        let len = {
-            let mut bytes = [0; 4];
-            env::read_slice(&mut bytes);
-            u32::from_le_bytes(bytes)
-        };
-        let mut input = vec![0u8; len as usize];
-        env::read_slice(&mut input);
-        input
-    }
-
-    fn commit_output(output: [u8; 32]) {
-        env::commit_slice(&output);
-    }
-
-    fn cycle_scope(_scope: ScopeMarker, _message: &str) {}
-}
+use ere_platform_risc0::Risc0Platform;
+use reth_guest::guest::ethereum_guest;
+use sha2::Sha256;
 
 /// Entry point.
 pub fn main() {
-    ethereum_guest::<Risc0SDK>();
+    ethereum_guest::<Risc0Platform<Sha256>>();
 }
