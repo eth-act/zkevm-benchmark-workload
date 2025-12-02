7613e5dc9e34b94a5416fe1cac7be164e0d5c90d
han0110 <tinghan0110@gmail.com>
2025-12-02 11:16:09 +0000

    feat: add crate `ethrex-guest` to implement `Guest` and update crates `ethrex-{zkvm}-stateless-validator` to use it

--- ere-guests/stateless-validator/ethrex/risc0/src/main.rs
+++ ere-guests/stateless-validator/ethrex/risc0/src/main.rs
@@ -1,60 +1,7 @@
-use ere_platform_risc0::{Platform, Risc0Platform, risc0_zkvm::guest::env};
-use k256::sha2::{Digest, Sha256};
+use ere_platform_risc0::Risc0Platform;
+use ethrex_guest::guest::{EthrexStatelessValidatorGuest, Guest};
+use k256::sha2::Sha256;
 
-use guest_program::{execution::execution_program, input::ProgramInput};
-use rkyv::rancor::Error;
-
-type P = Risc0Platform;
-
-fn main() {
-    println!("start reading input");
-    let start = env::cycle_count();
-    let input = P::read_whole_input();
-    let mut input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
-    let end = env::cycle_count();
-    eprintln!("reading input (cycle tracker): {}", end - start);
-
-    println!("public inputs preparation");
-    let start = env::cycle_count();
-    let block_hash = input.blocks[0].hash();
-    // Ethrex forces the block hash of any provided block to be zero, instead of accepting pre-calculated values
-    // and just checking them again.
-    input.blocks[0].header.hash = Default::default();
-    let parent_hash = input.blocks[0].header.parent_hash;
-    let end = env::cycle_count();
-    eprintln!("public inputs preparation (cycle tracker): {}", end - start);
-
-    println!("start stateless validation");
-    let start = env::cycle_count();
-    if input.blocks.len() != 1 {
-        commit_output(block_hash.0, parent_hash.0, false);
-        return;
-    }
-    let res = execution_program(input);
-    let end = env::cycle_count();
-    eprintln!("stateless validation (cycle tracker): {}", end - start);
-
-    println!("start commit public inputs");
-    let start = env::cycle_count();
-    // The public inputs are:
-    // - block_hash : [u8;32]
-    // - parent_hash : [u8;32]
-    // - successful_block_validation : bool
-    match res {
-        Ok(out) => {
-            commit_output(out.last_block_hash.0, parent_hash.0, true);
-        }
-        Err(_) => {
-            commit_output(block_hash.0, parent_hash.0, false);
-        }
-    }
-    let end = env::cycle_count();
-    eprintln!("commit public inputs (cycle tracker): {}", end - start);
-}
-
-fn commit_output(block_hash: [u8; 32], parent_hash: [u8; 32], is_valid: bool) {
-    let public_inputs = (block_hash, parent_hash, is_valid);
-    let public_inputs_hash: [u8; 32] =
-        Sha256::digest(bincode::serialize(&public_inputs).unwrap()).into();
-    P::write_whole_output(&public_inputs_hash);
+pub fn main() {
+    EthrexStatelessValidatorGuest::run::<Risc0Platform<Sha256>>();
 }
--- ere-guests/stateless-validator/ethrex/sp1/src/main.rs
+++ ere-guests/stateless-validator/ethrex/sp1/src/main.rs
@@ -1,51 +1,11 @@
 #![no_main]
 
-use ere_platform_sp1::{Platform, SP1Platform, sp1_zkvm};
-use guest_program::{execution::execution_program, input::ProgramInput};
-use k256::sha2::{Digest, Sha256};
-use rkyv::rancor::Error;
-
-type P = SP1Platform;
+use ere_platform_sp1::{sp1_zkvm, SP1Platform};
+use ethrex_guest::guest::{EthrexStatelessValidatorGuest, Guest};
+use k256::sha2::Sha256;
 
 sp1_zkvm::entrypoint!(main);
 
 pub fn main() {
-    P::cycle_scope_start("read_input");
-    let input = P::read_whole_input();
-    let mut input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
-    P::cycle_scope_end("read_input");
-
-    P::cycle_scope_start("public_inputs_preparation");
-    let block_hash = input.blocks[0].hash();
-    // Ethrex forces the block hash of any provided block to be zero, instead of accepting pre-calculated values
-    // and just checking them again.
-    input.blocks[0].header.hash = Default::default();
-    let parent_hash = input.blocks[0].header.parent_hash;
-    P::cycle_scope_end("public_inputs_preparation");
-
-    P::cycle_scope_start("validation");
-    if input.blocks.len() != 1 {
-        commit_output(block_hash.0, parent_hash.0, false);
-        return;
-    }
-    let res = execution_program(input);
-    P::cycle_scope_end("validation");
-
-    P::cycle_scope_start("commit_public_inputs");
-    match res {
-        Ok(out) => {
-            commit_output(out.last_block_hash.0, parent_hash.0, true);
-        }
-        Err(_) => {
-            commit_output(block_hash.0, parent_hash.0, false);
-        }
-    }
-    P::cycle_scope_end("commit_public_inputs");
-}
-
-fn commit_output(block_hash: [u8; 32], parent_hash: [u8; 32], is_valid: bool) {
-    let public_inputs = (block_hash, parent_hash, is_valid);
-    let public_inputs_hash: [u8; 32] =
-        Sha256::digest(bincode::serialize(&public_inputs).unwrap()).into();
-    P::write_whole_output(&public_inputs_hash);
+    EthrexStatelessValidatorGuest::run::<SP1Platform<Sha256>>();
 }
