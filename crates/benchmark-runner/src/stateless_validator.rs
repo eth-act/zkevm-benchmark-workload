0991cb36b80518de02d0a9ebe2c67e7e53fc8757
Ignacio Hagopian <jsign.uy@gmail.com>
2026-01-07 16:15:21 -0300

    refactor

--- crates/benchmark-runner/src/stateless_validator.rs
+++ crates/benchmark-runner/src/stateless_validator.rs
@@ -1,16 +1,22 @@
 //! Stateless validator guest program.
 
+use crate::guest_programs::GenericGuestFixture;
 use crate::guest_programs::GuestFixture;
-use anyhow::Result;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use serde::{Deserialize, Serialize};
+use anyhow::Context;
+use ere_guests_stateless_validator_reth::guest::{
+    StatelessValidatorOutput, StatelessValidatorRethGuest, StatelessValidatorRethInput,
+};
 use std::path::Path;
-use strum::{AsRefStr, EnumString};
-use walkdir::WalkDir;
 use witness_generator::StatelessValidationFixture;
 
-pub mod ethrex;
-pub mod reth;
+use anyhow::Result;
+use ere_guests_stateless_validator_ethrex::guest::{
+    StatelessValidatorEthrexGuest, StatelessValidatorEthrexInput,
+};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use serde::{Deserialize, Serialize};
+use strum::{AsRefStr, EnumString};
+use walkdir::WalkDir;
 
 /// Execution client variants.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, EnumString, AsRefStr)]
@@ -33,10 +39,74 @@ pub fn stateless_validator_inputs(
     input_folder: &Path,
     el: ExecutionClient,
 ) -> anyhow::Result<Vec<Box<dyn GuestFixture>>> {
+    let fixtures = read_benchmark_fixtures_folder(input_folder)?;
     match el {
-        ExecutionClient::Reth => reth::stateless_validator_inputs(input_folder),
-        ExecutionClient::Ethrex => ethrex::stateless_validator_inputs(input_folder),
+        ExecutionClient::Reth => reth_inputs_from_fixture(&fixtures),
+        ExecutionClient::Ethrex => ethrex_inputs_from_fixture(&fixtures),
     }
+}
+
+/// Create a vector of `GuestFixture` instances from `StatelessValidationFixture`.
+pub fn ethrex_inputs_from_fixture(
+    fixture: &[StatelessValidationFixture],
+) -> anyhow::Result<Vec<Box<dyn GuestFixture>>> {
+    fixture
+        .iter()
+        .map(|bw| {
+            let input = StatelessValidatorEthrexInput::new(&bw.stateless_input)
+                .context("Failed to create Ethrex stateless validator input")?;
+            let output = StatelessValidatorOutput::new(
+                bw.stateless_input.block.hash_slow(),
+                bw.stateless_input.block.parent_hash,
+                bw.success,
+            );
+            let metadata = BlockMetadata {
+                block_used_gas: bw.stateless_input.block.gas_used,
+            };
+
+            let fixture =
+                GenericGuestFixture::<BlockMetadata>::new::<StatelessValidatorEthrexGuest>(
+                    bw.name.clone(),
+                    input,
+                    output,
+                    metadata,
+                )
+                .output_sha256();
+
+            Ok(fixture.into_boxed())
+        })
+        .collect()
+}
+
+/// Create a vector of `GuestFixture` instances from `StatelessValidationFixture`.
+pub fn reth_inputs_from_fixture(
+    fixture: &[StatelessValidationFixture],
+) -> anyhow::Result<Vec<Box<dyn GuestFixture>>> {
+    fixture
+        .iter()
+        .map(|bw| {
+            let input = StatelessValidatorRethInput::new(&bw.stateless_input)
+                .context("Failed to create Reth stateless validator input")?;
+            let output = StatelessValidatorOutput::new(
+                bw.stateless_input.block.hash_slow(),
+                bw.stateless_input.block.parent_hash,
+                bw.success,
+            );
+            let metadata = BlockMetadata {
+                block_used_gas: bw.stateless_input.block.gas_used,
+            };
+
+            let fixture = GenericGuestFixture::<BlockMetadata>::new::<StatelessValidatorRethGuest>(
+                bw.name.clone(),
+                input,
+                output,
+                metadata,
+            )
+            .output_sha256();
+
+            Ok(fixture.into_boxed())
+        })
+        .collect()
 }
 
 /// Reads the benchmark fixtures folder and returns a list of block and witness pairs.
